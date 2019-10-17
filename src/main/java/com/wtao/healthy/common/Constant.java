450e923ce9104c32e445d8bc21de63c03d530e0f
wangtaowtao <298018972@qq.com>
2019-10-17 15:25:46 +0800

    添加常量

---
+++ src/main/java/com/wtao/healthy/common/Constant.java
@@ -0,0 +1,20 @@
+package com.wtao.healthy.common;
+
+/**
+ * Constant 常量
+ *
+ * @author wtao wangtao@eyaoshun.com
+ * @date 2019/10/17 15:20
+ */
+public class Constant {
+
+    /**
+     * 开启通知
+     */
+    public static final int OPRN_NOTIFY = 1;
+    /**
+     * 未开启通知
+     */
+    public static final int ON_OPRN_NOTIFY = 0;
+
+}
